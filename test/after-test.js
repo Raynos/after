29e1409ec916966ab7137b7f474c8c6280c5c6b8
Raynos <raynos2@gmail.com>
2012-01-29 18:35:28 +0000

    Refactored after, added forEach, map, reduce

--- test/after-test.js
+++ test/after-test.js
@@ -13,7 +13,122 @@ suite("After", function () {
         assert(after.some, "some");
     });
 
-    test("after when called with 0 invokes", function (done) {
-        after(0, done);
+    suite("after", function () {
+        test("after when called with 0 invokes", function (done) {
+            after(0, done);
+        });
+
+        test("after 1", function (done) {
+            var next = after(1, call(done));
+            next();
+        });
+
+        test("after 5", function (done) {
+            var next = after(5, call(done)), i = 5;
+            while (i--) {
+                next();
+            }
+        });
+
+        test("manipulate count", function (done) {
+            var next = after(1, call(done)), i = 5;
+
+            next.count = i;
+            while (i--) {
+                next();
+            }
+        });
+
+        test("after arguments", function (done) {
+            var next = after(2, function () {
+                assert(arguments[0][0] === data);
+                assert(arguments[1][0] === data);
+                assert(arguments[1][1] === data);
+                done();
+            }), data = {};
+
+            next(data);
+            next(data, data);
+        });
     });
+
+    var obj = {
+        "foo": "bar",
+        "foo1": "bar1",
+        "foo2": "bar2"
+    }
+
+    suite("iterators", function () {
+        test("multiple APIs", function (done) {
+            done = after(4, call(done));
+
+            after.forEach(obj, function (next) {
+                assert(typeof next === "function");
+                next();
+            }, done);
+
+            after.forEach(obj, function (value, next) {
+                assert(typeof next === "function");
+                next();
+            }, done);
+
+            after.forEach(obj, function (value, key, next) {
+                assert(typeof next === "function");
+                next();
+            }, done);
+
+            after.forEach(obj, function (value, key, obj, next) {
+                assert(typeof next === "function");
+                next();
+            }, done);
+        });
+
+        test("context", function (done) {
+            after.forEach(obj, function (next) {
+                assert(this === obj);
+                next();
+            }, obj, call(done));
+        })
+    });
+
+    suite("forEach", function () {
+        test("forEach on object", function (done) {
+            after.forEach(obj, function (value, key, next) {
+                assert(obj[key] === value);
+                next();
+            }, function () {
+                done(); 
+            });
+        });
+    });
+
+    suite("map", function () {
+        test("map on object", function (done) {
+            after.map(obj, function (value, next) {
+                next(value + value);
+            }, function (o) {
+                Object.keys(o).forEach(function (key) {
+                    assert((obj[key] + obj[key]) === o[key]);
+                });
+                done();
+            });
+        });
+    });
+
+    suite("reduce", function () {
+        test("reduce on object", function (done) {
+            after.reduce(obj, function (memo, value, next) {
+                next(memo + value);
+            }, function (str) {
+                assert(str === "barbar1bar2");
+                done();
+            });
+        });
+    })
 });
+
+function call(f) {
+    return function () {
+        f();
+    };
+}
