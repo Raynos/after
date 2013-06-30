8397d6fd5fb20852eb70fcf0ddd24361a2b8e394
Raynos <raynos2@gmail.com>
2013-06-29 23:08:13 -0700

    stuff

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 module.exports = after
 
 function after(count, callback, err_cb) {
-    var bail = false;
+    var bail = false
+    err_cb = err_cb || noop
     proxy.count = count
 
     return (count === 0) ? callback() : proxy
 
-    function proxy(err) {
+    function proxy(err, result) {
         if (proxy.count <= 0) {
-            throw new Error('after called too many times');
+            throw new Error('after called too many times')
         }
-        --proxy.count;
+        --proxy.count
 
         // after first error, rest are passed to err_cb
         if (err) {
             bail = true
-            callback(err);
+            callback(err)
             // future error callbacks will go to error handler
-            return callback = (err_cb || function() {})
+            callback = err_cb
+        } else if (proxy.count === 0 && !bail) {
+            callback(null, result)
         }
-        proxy.count === 0 && !bail && callback()
     }
 }
 
+function noop() {}
